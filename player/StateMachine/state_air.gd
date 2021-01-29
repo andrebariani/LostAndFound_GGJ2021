1a6957f55a45cf7e481922c1937b306fb3584f97
andrebariani <andrebariani@gmail.com>
2021-01-29 18:42:57 -0300

    dash ready

--- player/StateMachine/state_air.gd
+++ player/StateMachine/state_air.gd
@@ -12,7 +12,7 @@ func init():
 func run(_delta):
 	var dir = p.input_dir_vector
 	
-	if p.cooldowns.air_momentum.value != p.cooldowns.air_momentum.max_value:
+	if !p.is_on_wall():
 		if p.inputs.input_sprint:
 			p.velocity_move = p.approach(p.velocity_move, (p.max_speed + 200) * dir.x, p.air_acc)
 		else: 
@@ -34,11 +34,11 @@ func run(_delta):
 	if p.inputs.input_dash:
 		p.cooldowns.dash.value = 0
 		stateMachine.change_state("Dash")
-	elif p.is_on_wall() and p.CAN_WALLJUMP and p.velocity_jump > -90:
-		p.velocity_jump = 0
-		p.last_velocity_move_sign = sign(p.velocity_move)
-		p.cooldowns.walljump.value = 0
-		stateMachine.change_state("Walljump")
+	#elif p.is_on_wall() and p.CAN_WALLJUMP and p.velocity_jump > -90:
+	#	p.velocity_jump = 0
+	#	p.last_velocity_move_sign = sign(p.velocity_move)
+	#	p.cooldowns.walljump.value = 0
+	#	stateMachine.change_state("Walljump")
 	elif p.is_on_floor():
 		p.velocity_jump = 0
 		stateMachine.change_state("Idle")
