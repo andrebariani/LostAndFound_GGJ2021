eeace1a3c620bcce91ae4c5397cce7c0730863c6
MasterProjectLC <dan.inafuku@hotmail.com>
2021-01-30 19:46:34 -0300

    o2 e SPAAAACE

---
+++ objects/O2Area.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var player = null
+
+
+func _on_Area2D_body_entered(body):
+	if body.has_method("lose_oxygen"):
+		player = body
+		$Timer.start()
+
+
+func _on_Area2D_body_exited(body):
+	if body.has_method("fill_oxygen"):
+		body.fill_oxygen()
+		$Timer.stop()
+
+
+func _on_Timer_timeout():
+	if player != null:
+		player.lose_oxygen()
