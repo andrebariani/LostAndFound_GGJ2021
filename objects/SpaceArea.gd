7b7c9d2cc812762e75228d5c6f132d7c6ad6fa24
andrebariani <andrebariani@gmail.com>
2021-01-31 18:01:57 -0300

    bgm music and space effect added

--- objects/SpaceArea.gd
+++ objects/SpaceArea.gd
@@ -1,11 +1,16 @@
 extends "res://objects/O2Area.gd"
 
+var spaceEffect = AudioServer.get_bus_effect(1, 0)
 
 func _on_Area2D_body_entered(body):
 	if body.has_method("lose_oxygen"):
+		if spaceEffect:
+			spaceEffect.cutoff_hz = 1500
 		body.gravity_on = false
 
 
 func _on_Area2D_body_exited(body):
 	if body.has_method("lose_oxygen"):
+		if spaceEffect:
+			spaceEffect.cutoff_hz = 20000
 		body.gravity_on = true
