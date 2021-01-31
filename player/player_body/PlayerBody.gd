e2fa8b5b634008a6a531310155756b31394bb806
Anderson <ander.user@gmail.com>
2021-01-31 14:08:53 -0300

    Animação PlayerBody

--- player/player_body/PlayerBody.gd
+++ player/player_body/PlayerBody.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
-const LEGS_SWITCHER = "parameters/Legs/BlendLegs/blend_amount"
-const ARMS_SWITCHER = "parameters/Arms/BlendArms/blend_amount"
-const HOLD_SWITCHER = "parameters/Arms/BlendHold/blend_amount"
+const LEGS_SWITCHER = "parameters/Legs/StateMachine/playback"
+const ARMS_SWITCHER = "parameters/Arms/StateMachine/playback"
 
 export var player: NodePath = "../Player"
 onready var player_node = get_node_or_null(player)
@@ -11,22 +10,25 @@ onready var body_animation_tree_node = $BodyAnimationTree
 
 func _process(_delta):
 	if _player_info_is_valid():
-		_switch_animation(player_node.sm.state_curr, false)
+		_switch_animation(player_node.sm.state_curr, player_node.is_held())
 
 func _switch_animation(animation_name: String, holding: bool):
+	var selected_animation: String
+	
 	match animation_name:
 		"Walljump", "Air", "Dash":
-			body_animation_tree_node[LEGS_SWITCHER] = -1.0
-			body_animation_tree_node[ARMS_SWITCHER] = -1.0
+			selected_animation = "wall"
 		"Idle":
-			body_animation_tree_node[LEGS_SWITCHER] = 0.0
-			body_animation_tree_node[ARMS_SWITCHER] = 0.0
+			selected_animation = "idle"
 		"Move":
-			body_animation_tree_node[LEGS_SWITCHER] = 1.0
-			body_animation_tree_node[ARMS_SWITCHER] = 1.0
+			selected_animation = "walk"
 	
-	body_animation_tree_node[HOLD_SWITCHER] = 1.0 if holding else 0.0
+	body_animation_tree_node[LEGS_SWITCHER].travel(selected_animation)
+	
+	if holding: body_animation_tree_node[ARMS_SWITCHER].travel("arm_hold")
+	else: body_animation_tree_node[ARMS_SWITCHER].travel("arm_" + selected_animation)
+
 
 
 func _player_info_is_valid() -> bool:
-	return player_node != null and player_node.sm.state_curr != null # and player_node.has_method("is_held")
+	return player_node != null and player_node.sm.state_curr != null and player_node.has_method("is_held")
