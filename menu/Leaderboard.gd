52a612ef5d7642ceb98ba034b9beb3aece209dcc
MasterProjectLC <dan.inafuku@hotmail.com>
2021-02-25 08:52:46 -0300

    leaderboard update

--- menu/Leaderboard.gd
+++ menu/Leaderboard.gd
@@ -1,27 +1,27 @@
 extends Control
 
-var display_types = ["Dano","Tempo","Satisfacao","Score"]
+var display_types = ["dano","tempo","satisfacao","score"]
 var display_type = 3
 var jogadores = []
 
 class MyCustomSorter:
-	static func sort_Dano(a, b):
-		if a["Dano"] < b["Dano"]:
+	static func sort_dano(a, b):
+		if a["dano"] < b["dano"]:
 			return true
 		return false
 	
-	static func sort_Tempo(a, b):
-		if a["Tempo"] > b["Tempo"]:
+	static func sort_tempo(a, b):
+		if a["tempo"] > b["tempo"]:
 			return true
 		return false
 	
-	static func sort_Satisfacao(a, b):
-		if a["Satisfacao"] > b["Satisfacao"]:
+	static func sort_satisfacao(a, b):
+		if a["satisfacao"] > b["satisfacao"]:
 			return true
 		return false
 	
-	static func sort_Score(a, b):
-		if a["Score"] < b["Score"]:
+	static func sort_score(a, b):
+		if a["score"] < b["score"]:
 			return true
 		return false
 
@@ -53,5 +53,5 @@ func display():
 	jogadores.sort_custom(MyCustomSorter, "sort_" + display_types[display_type])
 	
 	for i in range(min(7, len(jogadores))):
-		$Jogadores.get_child(i).get_node("Nome").text = jogadores[i]["Nome"]
+		$Jogadores.get_child(i).get_node("Nome").text = jogadores[i]["nome"]
 		$Jogadores.get_child(i).get_node("Valor").text = jogadores[i][display_types[display_type]]
